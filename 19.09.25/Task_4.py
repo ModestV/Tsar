011f666edda40dc563f27dc5c2b96f2b19bd12ea
ModestV <vladpolovinka@mail.ru>
2025-10-16 03:57:37 +0500

    Update Task_4.py

--- 19.09.25/Task_4.py
+++ 19.09.25/Task_4.py
@@ -1,1 +1,16 @@
-
+def f(x):
+    s = set()
+    for i in range(2,int(x**0.5)+1):
+        if x%i==0:
+            s.add(i)
+            s.add(x//i)
+    return sorted(s)
+MaxNOD = -1
+# print(f(1260))
+# print(f(3400))
+a = f(int(input("Введите первое число (1260): ")))
+b = f(int(input("Введите второе число (3400): ")))
+for i in a:
+    if i in b:
+        if MaxNOD < i: MaxNOD = i
+print(MaxNOD)
