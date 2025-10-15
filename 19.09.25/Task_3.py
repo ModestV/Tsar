aec5cf2e965e2b11be927af9344e0f34e5b1b28d
ModestV <vladpolovinka@mail.ru>
2025-10-16 03:57:15 +0500

    Update Task_3.py

--- 19.09.25/Task_3.py
+++ 19.09.25/Task_3.py
@@ -1,1 +1,14 @@
-
+l = []
+n= int(input('Введите кол-во чисел в списке: '))
+for z in range(n):
+    l.append(int(input("Введите числа: ")))
+n=0
+for a in range(len(l)):
+    for b in range(a+1,len(l)):
+        for c in range(b+1,len(l)):
+            if l[a]+l[b]+l[c] == 0:
+                n+=1
+if n>0:
+    print('True')
+else:
+    print('False')
