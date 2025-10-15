e00b44755be0b7caba0107e70b4e463fcca03771
ModestV <vladpolovinka@mail.ru>
2025-10-16 04:00:24 +0500

    Update Task_3.py

--- 26.09.25/Task_3.py
+++ 26.09.25/Task_3.py
@@ -1,1 +1,9 @@
-
+# №3
+import random
+digits = random.choices('12345678',k=3)
+letters = random.choices('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM',k=3)
+symbols = random.choices('!@#$%^&*',k=2)
+len = digits + letters + symbols
+random.shuffle(len)
+password = ''.join(len)
+print(password)
