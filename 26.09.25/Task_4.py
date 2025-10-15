59950a79c98158474872f702e2a819d54f0f92af
ModestV <vladpolovinka@mail.ru>
2025-10-16 04:00:58 +0500

    Update Task_4.py

--- 26.09.25/Task_4.py
+++ 26.09.25/Task_4.py
@@ -1,1 +1,23 @@
+# №4
+a = input("Введите строку: ").lower()
+dict = {}
+for i in a:
+    if i in dict:
+        dict[i] += 1
+    else:
+        dict[i] = 1
 
+b = []
+
+for i, count in dict.items():
+    b.append((i, count))
+
+for i in range(len(b)):
+    for j in range(i + 1, len(b)):
+        if b[i][1] < b[j][1]:
+            b[i], b[j] = b[j], b[i]
+top = b[:3]
+if top:
+    for c in range(len(top)):
+        d, count = top[c]
+        print(f"Символ '{d}': {count} раз")
