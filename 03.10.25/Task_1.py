bc19c5cbfd704dc4776ed8d10019d4759988ad4d
ModestV <vladpolovinka@mail.ru>
2025-10-17 05:13:52 +0500

    Update Task_1.py

--- 03.10.25/Task_1.py
+++ 03.10.25/Task_1.py
@@ -1,1 +1,24 @@
+a = 'АБВГДЕЖЗИЙКЛМНОПРСТУФЧЦЧШЩЪЫЬЭЮЯ'.lower()
+b = 'abcdefghijklmnopqestuvwxyz'
+c = input('Введите чудестный текст: ').lower()
+d = int(input('Введите чудестный сдвиг: '))
 
+new_c = ''
+for i in c:
+    if i in a:
+        index_for_a = a.find(i)+d
+        if index_for_a > 31:
+            index_for_a-=32
+            new_c = new_c + a[index_for_a]
+        else: 
+            new_c = new_c + a[index_for_a]
+    elif i in b:
+        index_for_b = b.find(i)+d
+        if index_for_b > 24: 
+            index_for_b-=26
+            new_c = new_c + b[index_for_b]
+        else: 
+            new_c = new_c + b[index_for_b]
+    else:
+        new_c = new_c + i
+print(new_c)
