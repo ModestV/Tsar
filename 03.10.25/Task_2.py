7949860dfabf1c47929e890ae82d541f1c4810b7
ModestV <vladpolovinka@mail.ru>
2025-10-17 05:14:04 +0500

    Update Task_2.py

--- 03.10.25/Task_2.py
+++ 03.10.25/Task_2.py
@@ -1,1 +1,12 @@
-
+def check_winners(scores,student_score):
+    top3= (sorted(scores)[::-1])[:3]
+    if student_score in scores:
+        return('Вы в тройке победителей!')
+    else:
+        return('Вы не прошли в тройку победителей.')
+a = int(input('Сколько участников? '))
+l = []
+for i in range(a):
+    l.append(int(input('Введите бал участника: ')))
+b = int(input('Введите ваш бал: '))
+print(check_winners(l,b))
